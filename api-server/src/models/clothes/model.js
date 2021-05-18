1e56cae4037604731a6d8549b18245f4777e0736
Malak Al-Momani <malak.momani.96@gmail.com>
2021-05-18 13:46:29 +0300

    initialize

---
+++ api-server/src/models/clothes/model.js
@@ -0,0 +1,14 @@
+  
+'use strict';
+
+const mongoose = require('mongoose');
+
+const clothesSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  color: { type: String, required: true },
+  size: { type: String, required: true }
+});
+
+const clothesModel = mongoose.model('clothes', clothesSchema);
+
+module.exports = clothesModel;
